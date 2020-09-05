aba8e752f739fa6c98b09ebdeae586f1a2bf0490
Adel Stiti <adelw126@gmail.com>
2020-09-05 18:38:12 +0100

    Updates

---
+++ precache-manifest.06ef773487c183dab1396dd44751003f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35f18dd5ebb1f8de4144a6768ed37d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aebe830fd90c6201ec36",
+    "url": "/static/css/main.2d52a0d2.chunk.css"
+  },
+  {
+    "revision": "066e681405f49ddae920",
+    "url": "/static/js/2.cbd7da28.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.cbd7da28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aebe830fd90c6201ec36",
+    "url": "/static/js/main.7523531c.chunk.js"
+  },
+  {
+    "revision": "6ae1a7856111b565c38e",
+    "url": "/static/js/runtime-main.1f9c0018.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shoppingBag.30f07071.svg"
+  }
+]);
