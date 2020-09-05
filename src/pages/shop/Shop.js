ff82be30e730a3fb5aced5b6eac363a64d121bea
Adel Stiti <adelw126@gmail.com>
2020-09-05 19:24:46 +0100

    adding some features,fix bugs, add Readme.md

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,6 +1,5 @@
 import React,{useEffect,useState} from 'react'
 import {Route} from 'react-router-dom'
-
 import CollectionsOverview from '../../components/collectionsOverview/CollectionsOverview'
 import Category from '../category/Category'
 
